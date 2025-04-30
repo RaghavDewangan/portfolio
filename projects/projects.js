2fe3ab70ebd3baab6d65f088e8e0d0034c592126
Raghav Dewangan <raghav.dewangan2004@gmail.com>
2025-04-30 11:02:17 -0700

    added legend and pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,37 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, index) => {
+    d3.select('svg').append('path').attr('d', arc).attr('fill', colors(index));
+});
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('class', 'legend-item')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`<span class="swatch"></span> <span class="legend-label">${d.label}</span> <span class="legend-value">(${d.value})</span>`);
+});
