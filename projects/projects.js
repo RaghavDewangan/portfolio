0e8101a8d8e09f86106bfe8adf4f5d7d8f26bfa4
Raghav Dewangan <raghav.dewangan2004@gmail.com>
2025-05-01 11:33:28 -0700

    search bar not loading idk y

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,8 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+document.addEventListener('DOMContentLoaded', async () => {
+
 let projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 const searchInput = document.querySelector('.searchBar');
@@ -117,3 +119,4 @@ function applyFilters() {
 
     renderProjects(filteredProjects, projectsContainer, 'h2');
 }
+});
